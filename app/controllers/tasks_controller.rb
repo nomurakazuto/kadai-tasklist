2beda9c32eab1b093c8b5d2040ef0889136a5ba3
tanaka.taro <github_kadaiyou@outlook.jp>
2021-03-03 12:57:17 +0000

    third fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:edit, :show, :destroy, :update]
-    before_action :require_user_logged_in?, only: [:index, :new, :create]
+    
+    before_action :require_user_logged_in?
+    before_action :correct_user, only: [:edit, :show, :destroy, :update]
+    
     def index
         @tasks = current_user.tasks
     end
@@ -50,16 +52,16 @@ class TasksController < ApplicationController
     end
     
     private
-    def set_task
-      @task = current_user.tasks.find_by(id: params[:id])
-      unless @task
-        redirect_to root_url
-      end
-    end
     
     def task_params
         params.require(:task).permit(:content, :status)
     end
     
-    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      
+      unless @task
+        redirect_to root_url
+      end
+    end
 end
